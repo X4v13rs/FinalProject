0e3f433e8872e188e16d4c2aebb8cd130aa3d93b
X4v13rs <diablo.taz@gmail.com>
2020-04-13 21:25:57 -0400

    second commit

---
+++ src/main/java/entity/Student.java
@@ -0,0 +1,80 @@
+package entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+@NamedQueries({
+        @NamedQuery(name = "findAllStudents", query = "SELECT s FROM Student s")
+})
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private int contactNumber;
+    private String program;
+    private String DoB;
+
+    public Student(String name, String email, int contactNumber, String program, String DoB ) {
+       this.name = name;
+       this.email = email;
+       this.program = program;
+       this.contactNumber = contactNumber;
+       this.DoB  = DoB;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(String doB) {
+        DoB = doB;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
